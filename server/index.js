3af5ad05f1b974dbbae1ffa43dc81670f35b7a8f
mani-tammina <manikanta@tammina.com>
2023-02-20 16:31:43 +0530

    server code commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const api = require('./api');
+const mailer = require('./nodemailer');
 const port = 3000;
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', api);
+app.use('/email', mailer);
 
 app.use(express.static('../server/irp'))
 app.set('view engine', 'pug');
