1716781ddc5b8744ed8fa42c602eaa1bd699b105
saisuman-tammina <94602275+saisuman-tammina@users.noreply.github.com>
2023-02-21 11:01:44 +0530

    user forms
    Jobcreate and Apply job forms are done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -9,7 +8,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { JobcreateComponent } from './jobcreate/jobcreate.component';
 import { ApplyjobComponent } from './applyjob/applyjob.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
+import { MaterialExampleModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -18,13 +23,21 @@ import { HttpClientModule } from '@angular/common/http';
     RegisterComponent,
     DashboardComponent,
     JobcreateComponent,
-    ApplyjobComponent
+    ApplyjobComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     NgbModule,
     AppRoutingModule,
-    HttpClientModule
+    MaterialExampleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
